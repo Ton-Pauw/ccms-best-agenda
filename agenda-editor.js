45168127de32d2ac6d655046fe18c8a43384641a
Ton van Lankveld <Ton-Lankveld@users.noreply.github.com>
2015-11-27 18:09:22 +0100

    Create agenda-editor.js

---
+++ agenda-editor.js
@@ -0,0 +1,21 @@
+﻿/**
+ * @name Agenda Editor
+ * @description Edit the JSON file with the data of the Computer Club Medical Systems (CCMS) meetings.
+ * @author Ton van Lankveld (ton.van.lankveld@philips.com)
+ * @version 0.0.1 (2015-11-27)
+ *
+ * Used library: jQuery 1.11.3 (http://jquery.com/)
+ *               jQuery plugin: jquery.json 2.5.1 (https://github.com/krinkle/jquery-json)
+ *
+ * Documentation: JsDoc 3 Toolkit (http://usejsdoc.org/)
+ */
+
+
+
+/**
+ * @name Main loop
+ * @requires jQuery
+ */
+ "use strict";
+ $('section.error').hide();
+ 
