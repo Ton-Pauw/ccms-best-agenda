8e114167ff4654ae79fe37a93c86b9bfe2962b42
Ton van Lankveld <Ton-Lankveld@users.noreply.github.com>
2016-03-02 21:16:00 +0100

    Added tested functions

--- agenda-editor.js
+++ agenda-editor.js
@@ -2,7 +2,7 @@
  * @name Agenda Editor
  * @description Edit the JSON file with the data of the Computer Club Medical Systems (CCMS) meetings.
  * @author Ton van Lankveld (ton.van.lankveld@philips.com)
- * @version 0.0.1 (2015-11-27)
+ * @version 0.0.1 (2016-03-02)
  *
  * Used library: jQuery 1.11.3 (http://jquery.com/)
  *               jQuery plugin: jquery.json 2.5.1 (https://github.com/krinkle/jquery-json)
@@ -10,6 +10,88 @@
  * Documentation: JsDoc 3 Toolkit (http://usejsdoc.org/)
  */
 
+/**
+* @function
+* @name blinkServerIndicator
+* @description Let the server indictor blink one time.
+* @param {string} status - Server status: 'ok' | 'fault'
+* @returns None
+*/
+function blinkServerIndicator(status) {
+    "use strict"
+    if ((status === 'ok') || (status === 'fault')) {
+        var trans = $('#ServerIndicator').addClass(status);
+        setTimeout(function() {
+        trans.removeClass(status);
+    }, 1000);
+    }
+    return;
+}
+
+/**
+* @function
+* @name buildHTMLagendaTable
+* @description Build the HTML agenda table
+* @param {array} agendaArray - Matrix with the data of the meetings
+* @returns {string} HTMLout
+*/
+function buildHTMLagendaTable(agendaArray) {
+    "use strict";
+    var HTMLout = "";
+    var HTMLtableBegin = "<table>\n<caption>Data van de komende bijeenkomsten</caption>\n<thead>\n<tr><th></th><th scope=\"col\">Datum</th><th scope=\"col\">Tijd</th><th scope=\"col\">Onderwerp</th><th scope=\"col\">Groep</th><th scope=\"col\">Locatie</th><th scope=\"col\">Contactpersoon</th><th></th></tr>\n</thead>\n<tbody>";
+    var HTMLtbody = "";
+    var HTMLtableEnd = "</tbody>\n</table>";
+    var HTMLrowOdd = "";
+    var HTMLrowEven = "";
+    var oTRoTDRS2 = "<tr><td rowspan=\'2\'>";
+    var cTDoTDRS2 = "</td><td rowspan=\'2\'>";
+    var bttnEdit = "";
+    var bttnDel = "";
+    var meetingIso8601Str = "";
+    var meetingDateStr = "";
+    var SaST = ""; // Start and Stop Time of meeting
+
+    var NoR = agendaArray.length; // Number of Rows
+    var i = 0; // Row number
+    while (i < NoR) {
+        HTMLrowOdd = "";
+        HTMLrowEven = "";
+        bttnEdit = "<button id=\'edit" + (i+1) +  "\' type=\'button\'>Wijzig</button>";
+        bttnDel = "<button id=\'del" + (i+1) +  "\' type=\'button\'>Verwijder</button>";
+        meetingIso8601Str = (agendaArray[i]['start']).substring(0,10);
+        meetingDateStr = iso8601toStringNl(meetingIso8601Str);
+        SaST = (agendaArray[i]['start']).substring(11,16) + " - " + (agendaArray[i]['end']).substring(11,16) + " uur";
+        HTMLrowOdd = oTRoTDRS2 + bttnEdit + cTDoTDRS2 + meetingDateStr + cTDoTDRS2 + SaST + "</td><td>" + agendaArray[i]['onderwerp'] + "</td><td>" + agendaArray[i]['groep'] + cTDoTDRS2 + agendaArray[i]['location'] + "</td><td>" + agendaArray[i]['contact'] + cTDoTDRS2 + bttnDel + "</td></tr>\n";
+        HTMLrowEven = "<tr><td>" + agendaArray[i]['subject'] + "</td><td>" + agendaArray[i]['group'] + "</td><td>" + agendaArray[i]['email'] + "</td></tr>\n";
+        HTMLtbody = HTMLtbody + HTMLrowOdd + HTMLrowEven;
+        i += 1;
+    }
+    HTMLout = HTMLtableBegin + HTMLtbody + HTMLtableEnd;
+    return HTMLout;
+}
+
+/**
+* @function
+* @name deleteMeeting
+* @description Delete one row of the agenda
+* @param {array} agendaArray - Matrix with the data of the meetings
+* @param {int} rowNumber - Number of row to be deleted
+* @returns {string} agendaArray or EMPTYARRAY
+*/
+function deleteMeeting(agendaArray, rowNumber) {
+    "use strict";
+    var EMPTYARRAY = [];
+
+    var NoR = agendaArray.length;  // Number of Rows
+    if (NoR < (rowNumber - 1)) {
+        return agendaArray;
+    }
+    if (NoR < 1) {
+        return EMPTYARRAY;
+    }
+    agendaArray.splice(rowNumber, 1);
+    return agendaArray;
+}
 
 
 /**
